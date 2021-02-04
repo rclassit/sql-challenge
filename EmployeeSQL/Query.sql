21badaf971b1722b8f000878fe28f288c79f4a6a
Ryan Curt <rclassit@ncsu.edu>
2021-02-03 22:12:27 -0500

    #4

--- EmployeeSQL/Query.sql
+++ EmployeeSQL/Query.sql
@@ -16,7 +16,12 @@ INNER JOIN departments AS d
 LEFT JOIN employees AS e
 		ON dm.emp_no=e.emp_no;
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e 
+INNER JOIN dept_emp AS de
+		ON e.emp_no=de.emp_no
+INNER JOIN departments AS d
+		ON de.dept_no=d.dept_no;
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
