c0b053cc57d835ec7e354eab90ced6f9ff7075fd
Ryan Curt <rclassit@ncsu.edu>
2021-02-03 22:22:50 -0500

    finished query

--- EmployeeSQL/Query.sql
+++ EmployeeSQL/Query.sql
@@ -44,3 +44,7 @@ INNER JOIN departments AS d
 		ON de.dept_no=d.dept_no
 WHERE d.dept_name in ('Sales','Development');
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
